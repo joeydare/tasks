b691b61dc501cffee6b875f21404f05c6cc59483
Joey <joeydare@comcast.net>
2024-02-15 21:31:08 -0500

    Completed injectPositive function

--- src/arrays.ts
+++ src/arrays.ts
@@ -101,5 +101,14 @@ export function makeMath(addends: number[]): string {
  * And the array [1, 9, 7] would become [1, 9, 7, 17]
  */
 export function injectPositive(values: number[]): number[] {
-    return [];
+    const negIndex = values.findIndex((value) => value < 0);
+    if (negIndex === -1) {
+        return [...values, values.reduce((sum, currNum) => sum + currNum, 0)];
+    }
+    return [
+        ...values.slice(0, negIndex),
+        values[negIndex],
+        values.slice(0, negIndex).reduce((sum, currNum) => sum + currNum, 0),
+        ...values.slice(negIndex + 1)
+    ];
 }
