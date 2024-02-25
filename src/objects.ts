e8685dd8b9e3a95eb487300856410318d805cbd2
Joey <joeydare@comcast.net>
2024-02-25 13:31:14 -0500

    duplicateQuestion function

--- src/objects.ts
+++ src/objects.ts
@@ -120,7 +120,16 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    return {
+        id,
+        name: "Copy of " + oldQuestion.name,
+        body: oldQuestion.body,
+        type: oldQuestion.type,
+        options: oldQuestion.options,
+        expected: oldQuestion.expected,
+        points: oldQuestion.points,
+        published: false
+    };
 }
 
 /**
