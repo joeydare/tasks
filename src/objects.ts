8737dc3bb4e06481f885d024a0ec3c75733a9d04
Joey <joeydare@comcast.net>
2024-02-25 13:21:43 -0500

    toMarkdown function

--- src/objects.ts
+++ src/objects.ts
@@ -80,7 +80,14 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    let stringResult = "# " + question.name + "\n" + question.body;
+    if (question.type === "multiple_choice_question") {
+        const bodyOptions = question.options
+            .map((option) => "- " + option)
+            .join("\n");
+        stringResult += "\n" + bodyOptions;
+    }
+    return stringResult;
 }
 
 /**
