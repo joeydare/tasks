37bc02ab9bcae83a5e2d7b620b1201fefdef7bf3
Joey <joeydare@comcast.net>
2024-02-29 22:39:57 -0500

    Completed CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,71 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+enum Holiday {
+    Christmas = "🎁",
+    Halloween = "🎃",
+    Easter = "🐇",
+    stPatricks = "☘️",
+    Valentines = "💖"
+}
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [currentHoliday, setCurrentHoliday] = useState<Holiday>(
+        Holiday.Valentines
+    );
+    const advanceByAlphabet = (current: Holiday): Holiday => {
+        switch (current) {
+            case Holiday.Christmas:
+                return Holiday.Easter;
+            case Holiday.Easter:
+                return Holiday.Halloween;
+            case Holiday.Halloween:
+                return Holiday.stPatricks;
+            case Holiday.stPatricks:
+                return Holiday.Valentines;
+            case Holiday.Valentines:
+                return Holiday.Christmas;
+            default:
+                return current;
+        }
+    };
+
+    const advanceByYear = (current: Holiday): Holiday => {
+        switch (current) {
+            case Holiday.Valentines:
+                return Holiday.stPatricks;
+            case Holiday.stPatricks:
+                return Holiday.Easter;
+            case Holiday.Easter:
+                return Holiday.Halloween;
+            case Holiday.Halloween:
+                return Holiday.Christmas;
+            case Holiday.Christmas:
+                return Holiday.Valentines;
+            default:
+                return current;
+        }
+    };
+
+    const handleAlphabeticalClick = () => {
+        const nextHoliday = advanceByAlphabet(currentHoliday);
+        setCurrentHoliday(nextHoliday);
+    };
+
+    const handleYearlyClick = () => {
+        const nextHoliday = advanceByYear(currentHoliday);
+        setCurrentHoliday(nextHoliday);
+    };
+
+    return (
+        <div>
+            <div>Holiday: {currentHoliday}</div>
+            <Button variant="primary" onClick={handleAlphabeticalClick}>
+                Advance by Alphabet
+            </Button>
+            <Button variant="secondary" onClick={handleYearlyClick}>
+                Advance by Year
+            </Button>
+        </div>
+    );
 }
