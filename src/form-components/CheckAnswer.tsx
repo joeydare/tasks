a12e8d65b35456fff72c067afa83e6618c8e1584
Joey <joeydare@comcast.net>
2024-03-04 14:38:36 -0500

    Check Answer completed

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,28 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState<string>("");
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(event.target.value);
+    };
     return (
         <div>
             <h3>Check Answer</h3>
+            <form>
+                <label>
+                    Your Answer:
+                    <input
+                        type="text"
+                        value={userAnswer}
+                        onChange={handleChange}
+                    />
+                </label>
+            </form>
+            {userAnswer === expectedAnswer ? (
+                <p>✔️ Correct!</p>
+            ) : (
+                <p>❌ Incorrect</p>
+            )}
         </div>
     );
 }
